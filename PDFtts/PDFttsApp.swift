802207642913cbed9d85ac37e09db22ae12f10be
Your Name <your_email@example.com>
2025-07-16 14:58:28 +0800

    Initial Commit

---
+++ PDFtts/PDFttsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFttsApp.swift
+//  PDFtts
+//
+//  Created by Matt on 16/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct PDFttsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PDFttsTests/PDFttsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PDFttsTests.swift
+//  PDFttsTests
+//
+//  Created by Matt on 16/7/2025.
+//
+
+import Testing
+@testable import PDFtts
+
+struct PDFttsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
